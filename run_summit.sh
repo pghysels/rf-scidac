02c2cbc912ce42d8b8cef32a44545d33a8a15df7
Pieter Ghysels <pghysels@lbl.gov>
2022-06-17 21:34:24 -0700

    Update run_summit.sh

--- run_summit.sh
+++ run_summit.sh
@@ -11,6 +11,12 @@ module unload darshan-runtime
 
 export OMP_NUM_THREADS=7
 
+# 64 nodes x 42 cores = 2688 cores total
+# every node has 6 GPU's, use 6 resource sets per node (-r 6)
+# 1 GPU per resource set (-g 1)
+# use 7 threads (cores) per resource set (-c 7  -d packed -b packed:7)
+# 7 cores x 6 resource sets = 42 cores per node
+# 6 resource sets per node x 64 nodes = 384 MPI ranks
 jsrun -n 384 -a 1 -c 7 -g 1 -r 6 -l CPU-CPU -d packed -b packed:7 \
       ./build/main \
       /gpfs/alpine/scratch/pghysels/csc289/rf-scidac/matrix. \
