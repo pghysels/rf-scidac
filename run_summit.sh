440230e3c0e05c4d3548fac1de14b582e84d13ea
Pieter Ghysels <pghysels@lbl.gov>
2022-06-18 00:27:44 -0400

    Update scripts, README, output on Summit

--- run_summit.sh
+++ run_summit.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Begin LSF Directives
 #BSUB -P CSC289
-#BSUB -W 01:00
+#BSUB -W 00:30
 #BSUB -nnodes 64
 #BSUB -J rf-scidac
 #BSUB -o rf-scidac.%J
 #BSUB -e rf-scidac.%J
 
 module unload darshan-runtime
-
 
 export OMP_NUM_THREADS=7
 
@@ -18,14 +17,15 @@ jsrun -n 384 -a 1 -c 7 -g 1 -r 6 -l CPU-CPU -d packed -b packed:7 \
       /gpfs/alpine/scratch/pghysels/csc289/rf-scidac/rhs_0.000000 \
       --sp_compression none \
       --sp_matching 0 \
-      --sp_disable_gpu \
-      --blr_rel_tol 1e-2 \
-      --hodlr_butterfly_levels 100 \
-      --hodlr_rel_tol 1e-2 \
-      --sp_hodlr_min_sep_size 10000000 \
+      --sp_enable_gpu \
       --sp_reordering_method parmetis \
-      --sp_print_compressed_front_stats \
-      --help \
-      > out/order3_summit_N64_none_parmetis_rhs_CPU.log
+      --help > out/order3_summit_N64_none_parmetis_GPU.log
 
-#      --blr_factor_algorithm COLWISE \
+
+# --blr_rel_tol 1e-4 \
+# --hodlr_butterfly_levels 100 \
+# --hodlr_rel_tol 1e-4 \
+# --sp_hodlr_min_sep_size 10000000 \
+# --sp_print_compressed_front_stats \
+
+# --blr_factor_algorithm COLWISE \
